7fd9316e94ca446b19076168d76dd8f951de9d56
Ajithprasad Babu <ajithprasad.babu@gmail.com>
2023-01-26 20:29:17 +0000

    InitialCommit for char

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -4,3 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class Character:
     name: str
+    DEFAULT_CHAR_NAME = "Viper"
+    def __init__(self, name=DEFAULT_CHAR_NAME):
+        self.name = name
+        self.cPos = None
