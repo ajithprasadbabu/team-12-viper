b93b59824618a9b493c72574478bbfb44cd4d065
Bruce <brucejohnsonallen@gmail.com>
2023-01-26 20:49:07 +0000

    .

--- src/levelup/game_map.py
+++ src/levelup/game_map.py
@@ -1,13 +1,18 @@
 from dataclasses import dataclass
 
 
-@dataclass
 class GameMap:
-    row: int
-    col: int
+    def __init__(self):
+        self.row: int = 4
+        self.col: int = 4
 
-    def calculatePosition(self, direction):
-        pass
+    def calculatePosition(self, position, direction):
+        if direction == "n":
+            pass
+        elif direction == "s":
+            pass  
 
-    def validatePosition(self, direction):
-        pass
+    def validatePosition(self, position):
+        self.row = position.x
+        self.col = position.y
+        return col >= 0 and col < 10 and row >= 0 and row < 10 
