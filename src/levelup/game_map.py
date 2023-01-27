ee315bc971aece68288964d0fb82b246802c23ac
Bruce <brucejohnsonallen@gmail.com>
2023-01-27 14:56:59 +0000

    .

--- src/levelup/game_map.py
+++ src/levelup/game_map.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 
+MIN_ROW = 0
+MAX_ROW = 9
+MIN_COL = 0
+MAX_COL = 9
+
 
 class GameMap:
     def __init__(self):
@@ -7,12 +12,28 @@ class GameMap:
         self.col: int = 4
 
     def calculatePosition(self, position, direction):
-        if direction == "n":
-            pass
-        elif direction == "s":
-            pass  
-
-    def validatePosition(self, position):
         self.row = position.x
         self.col = position.y
-        return col >= 0 and col < 10 and row >= 0 and row < 10 
+        
+        if direction == "n":
+            row = self.row - 1
+            if self.validatePosition(row, self.col):
+                self.row = row
+
+        elif direction == "s":
+            row = self.row + 1
+            if self.validatePosition(row, self.col):
+                self.row = row
+
+        elif direction == "e":
+            col = self.col + 1
+            if self.validatePosition(self.row, col):
+                self.col = col
+
+        elif direction == "w":
+            col = self.col - 1
+            if self.validatePosition(self.row, col):
+                self.col = col 
+
+    def validatePosition(self, row, col):
+        return col >= MIN_COL and col < MAX_COL and row >= MIN_ROW and row <= MAX_ROW 
